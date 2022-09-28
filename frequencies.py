3e1f8a02887fea7920356fa3d584b75ca4f17b9d
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 18:26:02 +0100

    Add solution to frequency.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    strings = [str(x) for x in items]
+    for i in strings:
+
+        if i in frequencies :
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
     return frequencies
